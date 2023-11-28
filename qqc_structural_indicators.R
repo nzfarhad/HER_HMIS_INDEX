ed94d10eee641a250f1ec4d832c7d48f29ea1267
Sayed Farhad Nabizada <nzfarhad@gmail.com>
2023-11-28 13:11:59 +0500

    Added 2.Hygiene

--- qqc_structural_indicators.R
+++ qqc_structural_indicators.R
@@ -11,6 +11,7 @@ data <- read_excel(data_path, sheet = "data", guess_max = 5000000)
 
 data_processed <- data %>% 
   mutate(
+    # 1. Gen_Management
     q1_1_gen_mgmnt = case_when(
       q1_1_1 == 1 & q1_1_2 == 1 & q1_1_3 == 1 & q1_1_4 == 1 & q1_1_5 ~ 0.5,
       TRUE ~ 0
@@ -66,9 +67,92 @@ data_processed <- data %>%
       q1_9 %in% "Yes" ~ 2,
       TRUE ~ 0
     ),
+    
+    # 2. Hygiene
+    q2_1_hygiene  = case_when(
+      q2_1 %in% "Yes" ~ 1,
+      TRUE ~ 0
+    ),
+    
+    q2_2_hygiene  = case_when(
+      q2_2 %in% "Yes" ~ 1,
+      TRUE ~ 0
+    ),
+    
+    q2_3_1_hygiene  = case_when(
+      q2_3_1_1 == 1 & q2_3_1_2 == 1 & q2_3_1_3 == 1 & q2_3_1_4 == 1 ~ 1,
+      TRUE ~ 0
+    ),
+    
+    q2_3_2_hygiene  = case_when(
+      q2_3_2 %in% "Yes" ~ 0.5,
+      TRUE ~ 0
+    ),
+    
+    q2_3_3_hygiene  = case_when(
+      q2_3_3 %in% "Yes" ~ 0.5,
+      TRUE ~ 0
+    ),
+    
+    q2_3_4_hygiene  = case_when(
+      q2_3_4_1 == 1 & q2_3_4_2 == 1 & q2_3_4_3 == 1 & q2_3_4_4 == 1 ~ 0.5,
+      TRUE ~ 0
+    ),
+    
+    q2_3_5_hygiene  = case_when(
+      q2_3_5_1 == 1 & q2_3_5_2 == 1 & q2_3_5_3 == 1 ~ 0.5,
+      TRUE ~ 0
+    ),
+    
+    q2_4_1_hygiene  = case_when(
+      q2_4_1_1 == 1 & q2_4_1_2 == 1 & q2_4_1_3 == 1 ~ 1,
+      TRUE ~ 0
+    ),
+    
+    q2_4_2_hygiene  = case_when(
+      q2_4_2 %in% "Yes" ~ 0.5,
+      TRUE ~ 0
+    ),
+    
+    q2_4_3_hygiene  = case_when(
+      q2_4_3 %in% "Yes" ~ 0.5,
+      TRUE ~ 0
+    ),
+    
+    q2_5_hygiene  = case_when(
+      q2_5_1 == 1 & q2_5_2 == 1 & q2_5_3 == 1 & q2_5_4 == 1 ~ 12,
+      TRUE ~ 0
+    ),
+    
+    q2_6_hygiene  = case_when(
+      q2_6 %in% "Yes" ~ 1,
+      TRUE ~ 0
+    ),
+    
+    q2_7_hygiene  = case_when(
+      q2_7_1 == 1 & q2_7_2 == 1 & q2_7_3 == 1 & q2_7_4 == 1 & q2_7_5 == 1 ~ 3,
+      TRUE ~ 0
+    ),
+    
+    q2_8_hygiene  = case_when(
+      q2_8_1 == 1 & q2_8_2 == 1 & q2_8_3 == 1 & q2_8_4 == 1 ~ 2,
+      TRUE ~ 0
+    ),
+    
+    q2_9_hygiene  = case_when(
+      q2_9_1 == 1 & q2_9_2 == 1 & q2_9_3 == 1 & q2_9_4 == 1 ~ 6,
+      TRUE ~ 0
+    ),
+    
+    q2_10_hygiene  = case_when(
+      q2_10_1 == 1 & q2_10_2 == 1 & q2_10_3 == 1 ~ 3,
+      TRUE ~ 0
+    ),
+    
+    
   )
 
 
-table(data_processed$q1_9)
+table(data_processed$q2_10_hygiene)
 table(is.na(data_processed$q1_5))
 
